3a4a5ac8bacaa8628e18b3814335ded6d93fd322
sanjaysanjup <sanjaysanjup003@gmail.com>
2025-11-12 17:23:31 +0530

    Update cls1.py
    there were some errors while running the programm

--- cls1.py
+++ cls1.py
@@ -1,51 +1,45 @@
 class Rectangle:
-    def __init__(self,length,width):
-        self.length=length
-        self.width=width
+    def __init__(self, length, breadth):
+        self.length = length
+        self.breadth = breadth
+
     def area(self):
-        return self.length*self.width
+        return self.length * self.breadth
+
     def perimeter(self):
-        return self.length+self.width
-
+        return self.length + self.breadth
     
-l=float(input("Enter length:"))
-w=float(input("Enter Width :"))
+len1 = float(input("Enter the length of rectangle 1:"))
+bre1 = float(input("Enter bredth of the rectangle 1:"))
 
-rect1 = Rectangle(l,w)
+rect1 = Rectangle(len1,bre1)
 
-rectp = rect.perimeter()
-recta = rect.area()
+rect1a = rect1.area()
+rect1p = rect1.perimeter()
 
-print("Perimeter rect1:",rectp)
-print("area rect1:",recta)
+print("\nArea of Rectangle 1:", rect1a)
+print("Perimeter of Rectangle 1:", rect1p)
 
-l2=float(input('Enter length '))
-w2=float(input("ENTER Width :"))
 
-rect2 = Rectangle(l2,w2)
+len2 = float(input("\nEnter the length of rectangle 2:"))
+bre2 = float(input("Enter the bredth of rectangle 2:"))
 
-rect2p = rect2.perimeter()
+rect2 = Rectangle(len2,bre2)
+
 rect2a = rect2.area()
+rect2p = rect2.perimeter()
 
-print("Perimeter rect1:",rect2p)
-print("area rect1:",rect2a)
 
-if(rect2p<rectp):
-    print("Rectangle 1 is greater")
-elif rect2p>rectp:
-    print("Rectangle 2 is greater")
+print("\nArea of Rectangle 2:", rect2a)
+print("Perimeter of Rectangle 2:", rect2p)
+
+
+if(rect2a<rect1a):
+    print("\nRectangle 1 has greater area")
+elif rect2a>rect1a:
+    print("\nRectangle 2 has  greater area")
 else:
-    print("Both are same")
-
-
-
-
-
-
-
-
-
-
+    print("\nBoth are same")
 
 
 
